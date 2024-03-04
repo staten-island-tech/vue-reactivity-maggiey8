bfb971dd8620c474732516cebc116f3eefa4ae41
maggiey8 <yangmaggie2006@gmail.com>
2024-03-03 23:56:55 -0500

    done

---
+++ project/src/views/RankView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="rank">
+    <RankData v-for="items in store.chars" :Char = items></RankData>
+  </div>
+</template>
+
+<script setup>
+import RankData from '@/components/RankData.vue';
+import { useCharacterStore } from '@/stores/counter.js';
+
+const store = useCharacterStore()
+</script>
+
+<style>
+.rank {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}  
+</style>
